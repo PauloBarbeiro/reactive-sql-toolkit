6c892293b95be926c843c3a088cf7d7148b9e3ca
paulo.barbeiro <msg@paulobf.dev>
2022-04-05 22:06:25 +0200

    implement more cores functionality and tests

--- src/core/SQL/index.test.ts
+++ src/core/SQL/index.test.ts
@@ -1,33 +1,100 @@
-import { createSQL, getDatabase, WasmSources } from "./index";
+import { createSQL, executeQuery, WasmSources, database } from "./index";
 
 describe('Create Database to initialise the library', function () {
-    it('should initialize a database instance', async () => {
-        const db = await createSQL(WasmSources.test)
+    describe('createSQL', () => {
+        it('should initialize a database instance', async () => {
+            const db = await createSQL(WasmSources.test)
 
-        expect(db).toBeTruthy()
-        expect(db).toHaveProperty('exec')
-    }, 5000)
+            expect(db).toBeTruthy()
+            expect(db).toHaveProperty('exec')
+        }, 5000)
 
-    it('should execute a SQL query', async () => {
-        const db = await createSQL(WasmSources.test)
+        it('should execute a SQL query', async () => {
+            const db = await createSQL(WasmSources.test)
 
-        const result = db.exec(
-            "DROP TABLE IF EXISTS test;\n"
-            + "CREATE TABLE test (id INTEGER, age INTEGER, name TEXT);"
-            + "INSERT INTO test VALUES ($id1, :age1, @name1);"
-            + "INSERT INTO test VALUES ($id2, :age2, @name2);"
-            + "SELECT id FROM test;"
-            + "SELECT age,name FROM test WHERE id=$id1",
-            {
-                "$id1": 1, ":age1": 1, "@name1": "Ling",
-                "$id2": 2, ":age2": 18, "@name2": "Paul"
-            }
-        );
+            const result = db!.exec(
+                "DROP TABLE IF EXISTS test;\n"
+                + "CREATE TABLE test (id INTEGER, age INTEGER, name TEXT);"
+                + "INSERT INTO test VALUES ($id1, :age1, @name1);"
+                + "INSERT INTO test VALUES ($id2, :age2, @name2);"
+                + "SELECT id FROM test;"
+                + "SELECT age,name FROM test WHERE id=$id1",
+                {
+                    "$id1": 1, ":age1": 1, "@name1": "Ling",
+                    "$id2": 2, ":age2": 18, "@name2": "Paul"
+                }
+            );
 
-        expect(result).toEqual([
-            {columns:['id'],values:[[1],[2]]},
-            {columns:["age","name"],values:[[1,"Ling"]]},
-        ])
-    }, 5000)
+            expect(result).toEqual([
+                {columns:['id'],values:[[1],[2]]},
+                {columns:["age","name"],values:[[1,"Ling"]]},
+            ])
+        }, 5000)
+    })
 
+    describe('executeQuery', function () {
+        beforeAll(() => {
+            jest.resetAllMocks()
+            database.destroy()
+        })
+
+        afterEach(() => {
+            jest.resetAllMocks()
+        })
+
+        it('should log and error because db was not initialized', () => {
+            const test = jest.fn()
+            const spyOnLogError = jest.spyOn(console, 'error')
+            const result = executeQuery('SELECT * FROM test')
+            expect(spyOnLogError).toHaveBeenCalledWith('SQL-lite instance not initiated! \nRun createSQL function to initialize the service.')
+            expect(result).toEqual(undefined)
+        })
+
+        it('should execute a simple query', async () => {
+            const db = await createSQL(WasmSources.test)
+
+            db!.exec(
+                "DROP TABLE IF EXISTS test;\n"
+                + "CREATE TABLE test (id INTEGER, age INTEGER, name TEXT);"
+                + "INSERT INTO test VALUES (1, 10, 'Ling');"
+                + "INSERT INTO test VALUES (2, 18, 'Paul');"
+            );
+
+            const spyOnLogError = jest.spyOn(console, 'error')
+            const result = executeQuery('SELECT * FROM test')
+            expect(spyOnLogError).not.toHaveBeenCalled()
+            expect(result).toEqual([{"columns": ["id", "age", "name"], "values": [[1, 10, "Ling"], [2, 18, "Paul"]]}])
+        })
+
+        it('should execute a query with params', async () => {
+            const db = await createSQL(WasmSources.test)
+
+            db!.exec(
+                "DROP TABLE IF EXISTS test;\n"
+                + "CREATE TABLE test (id INTEGER, age INTEGER, name TEXT);"
+                + "INSERT INTO test VALUES (1, 10, 'Ling');"
+                + "INSERT INTO test VALUES (2, 18, 'Paul');"
+            );
+
+            const spyOnLogError = jest.spyOn(console, 'error')
+            const result = executeQuery(
+                "SELECT age,name FROM test WHERE id=$id1",
+                {
+                    "$id1": 2,
+                })
+            expect(spyOnLogError).not.toHaveBeenCalled()
+            expect(result).toEqual([{"columns": ["age", "name"], "values": [[18, "Paul"]]}])
+        })
+
+        it('should return empty results', async () => {
+            const db = await createSQL(WasmSources.test)
+
+            const spyOnLogError = jest.spyOn(console, 'error')
+            const result = executeQuery(
+                "SELECT age,name FROM test",
+                )
+            expect(spyOnLogError).toHaveBeenCalledWith('SQL-lite Error: no such table: test')
+            expect(result).toEqual(undefined)
+        })
+    });
 });
--- src/core/SQL/index.ts
+++ src/core/SQL/index.ts
@@ -6,13 +6,36 @@ export enum WasmSources {
     test = 'node_modules/sql.js/dist/sql-wasm.wasm'
 }
 
-const database = {
+export type SqlValue = string | number | null | Uint8Array
+
+export interface  QueryExecResult {
+    columns: Array<string>
+    values: Array<Array<SqlValue>>
+}
+
+export type BindParams = Array<SqlValue> | Record<string, SqlValue> | null
+
+export interface SqlLite {
+    exec: (query:string, params?: BindParams) => QueryExecResult
+}
+
+export interface DatabaseHolder {
+    instance: SqlLite | null
+    setInstance: (bd: SqlLite) => void
+    getInstance: () => SqlLite | null
+    destroy: () => void
+}
+
+export const database: DatabaseHolder = {
     instance: null,
     setInstance: function(db) {
         this.instance = db
     },
     getInstance: function() {
         return this.instance
+    },
+    destroy: function () {
+        this.instance = null
     }
 }
 
@@ -22,18 +45,35 @@ export const createSQL = async (path: string, name?: string) => {
             locateFile: () => path
         })
 
-        const db = new SQL.Database()
+        const db: SqlLite = new SQL.Database()
         database.setInstance(db)
         return db
     } catch (e) {
         console.error(e)
     }
-
+    return
 }
 
-export const getDatabase = () => database.getInstance()
+export const getDatabase = (): SqlLite | null => database.getInstance()
+
+export const executeQuery = (query: string, params?: BindParams): QueryExecResult | undefined => {
+    let db = getDatabase()
+
+    if(!db) {
+        console.error('SQL-lite instance not initiated! \nRun createSQL function to initialize the service.')
+        return;
+    }
+
+    try {
+        return db.exec(query, params)
+    } catch (error) {
+        console.error(`SQL-lite Error: ${error.message}`)
+        return
+    }
+}
 
 export default {
     createSQL,
-    getDatabase
+    // getDatabase,
+    executeQuery
 }
