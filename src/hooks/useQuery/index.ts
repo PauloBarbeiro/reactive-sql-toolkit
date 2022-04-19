c64eaf1e8c515a50344a044f95d20b51aab6488d
paulo.barbeiro <msg@paulobf.dev>
2022-04-19 23:07:31 +0200

    fixes useQuery code

--- src/hooks/useQuery/index.ts
+++ src/hooks/useQuery/index.ts
@@ -1,11 +1,11 @@
-import {useCallback, useState} from 'react'
-import {insertQueryPipeline, queryPipeline} from '../../core/SQL'
+import { useCallback, useState } from 'react'
+import { insertQueryPipeline, queryPipeline } from '../../core/SQL'
 
 export function useQuery(query: string, depths?: Array<string | number| boolean | undefined | null>) {
     const [updatedAt, setUpdatedAt] = useState<number>()
 
     const writeQueryFn = useCallback((query: string) => {
-        insertQueryPipeline(/*setUpdatedAt,*/ query)
+        insertQueryPipeline(query)
     }, [])
 
     return {
